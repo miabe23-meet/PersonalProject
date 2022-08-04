52fef2d373b68eedce0c90424c85f9f99ae43117
miabe23-meet <109899471+miabe23-meet@users.noreply.github.com>
2022-08-04 09:51:37 +0300

    project finished

--- app.py
+++ app.py
@@ -20,6 +20,56 @@ firebase=pyrebase.initialize_app(config)
 auth=firebase.auth()
 db=firebase.database()
 
+
+
+
+@app.route('/')
+def home():
+		if login_session['user'] is None:
+		 	name = {"name":"user"}
+		 	ok=True
+		else:
+			name=db.child("users").child(login_session['user']['localId']).get().val()
+			ok=False
+		return render_template("index.html",name = name, display= ok)
+
+
+@app.route('/about')
+def about():
+	if login_session['user'] is None:
+	 	name = {"name":"user"}
+	 	ok=True
+	else:
+		name=db.child("users").child(login_session['user']['localId']).get().val()
+		ok=False
+	return render_template("about.html" , name=name, display=ok)
+
+
+@app.route('/signUp' , methods=['GET', 'POST'])
+def signUp():
+	if login_session['user'] is None:
+				name = {"name":"user"}
+				ok=True
+	else:
+				name=db.child("users").child(login_session['user']['localId']).get().val()
+				ok=False
+	error = ""
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
+		try:
+			login_session['user'] = auth.create_user_with_email_and_password(email, password)
+			user={
+			"name": request.form['name'] ,
+			"email": request.form['email'] ,
+			"password": request.form['password']
+			}
+			db.child("users").child(login_session['user']['localId']).set(user)
+			return redirect(url_for('home'))
+		except:
+			error = "Authentication failed"
+	return render_template("signUp.html", name=name , display=ok)
+
 @app.route('/signIn' , methods=['GET', 'POST'])
 def signIn():
 	if login_session['user'] is None:
@@ -40,61 +90,7 @@ def signIn():
 			return redirect(url_for('home'))
 		except:
 			error = "Authentication failed"
-	return render_template("signin.html", name=name)
-
-
-@app.route('/')
-def home():
-	if 'user' in login_session:
-		if login_session['user'] is None:
-		 	name = {"name":"user"}
-		 	ok=True
-		else:
-			name=db.child("users").child(login_session['user']['localId']).get().val()
-			ok=False
-	else:
-		name=db.child("users").child(login_session['user']['localId']).get().val()
-		ok=False
-	return render_template("index.html",name = name, display= ok)
-
-
-@app.route('/about')
-def about():
-	if login_session['user'] is None:
-	 	name = {"name":"user"}
-	 	ok=True
-	else:
-		name=db.child("users").child(login_session['user']['localId']).get().val()
-		ok=False
-	return render_template("about.html" , name=name, display=ok)
-
-
-@app.route('/signUp' , methods=['GET', 'POST'])
-def signUp():
-	if login_session['user'] is None:
-	 	name = {"name":"user"}
-	 	ok=True
-	else:
-		name=db.child("users").child(login_session['user']['localId']).get().val()
-		ok=False
-	error = ""
-	if request.method == 'POST':
-		email = request.form['email']
-		password = request.form['password']
-		try:
-			login_session['user'] = auth.create_user_with_email_and_password(email, password)
-			user={
-			"name": request.form['name'] ,
-			"email": request.form['email'] ,
-			"password": request.form['password']
-			}
-			db.child("users").child(login_session['user']['localId']).set(user)
-			return redirect(url_for('home'))
-		except:
-			error = "Authentication failed"
-	return render_template("signUp.html", name=name , display=ok)
-
-
+	return render_template("signin.html", name=name , display=ok)
 
 
 @app.route('/post')
